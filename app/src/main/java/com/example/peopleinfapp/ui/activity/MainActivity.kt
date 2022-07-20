51505d64bc9e684e3010c2c1cc0941804d52c7a4
adelwww <wsnezka@gmail.com>
2022-07-20 19:10:11 +0300

    minor changes

--- app/src/main/java/com/example/peopleinfapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/peopleinfapp/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.peopleinfapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.peopleinfapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,5 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private val navController: NavController by lazy(LazyThreadSafetyMode.NONE) {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navHostFragment.navController
     }
 }
